1b8b0de6fb4c3b3b867d7679d27a9be1ed5810e7
continuationc <continuationc@gmail.com>
2025-03-19 10:53:33 +0800

    feat: init

--- src/main.tsx
+++ src/main.tsx
@@ -1,9 +1,9 @@
-import React from "react";
-import ReactDOM from "react-dom/client";
-import App from "./App";
+import Model from 'react-dom/client'
+import React from 'react'
+import Index from './lib'
 
-ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
+Model.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <App />
+    <Index />
   </React.StrictMode>,
-);
+)

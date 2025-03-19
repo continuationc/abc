18267706e07b10add86691e43275dd47fdde116d
continuationc <continuationc@gmail.com>
2025-03-19 10:53:33 +0800

    feat: init

--- src/main.tsx
+++ src/main.tsx
@@ -1,9 +1,4 @@
-import React from "react";
-import ReactDOM from "react-dom/client";
-import App from "./App";
+import Mod from 'react-dom/client'
+import App from '@/lib'
 
-ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-);
+Mod.createRoot(document.getElementById('root')!).render(<App />)
---
+++ teconfig.js
@@ -0,0 +1,23 @@
+/** @type {import('vite').UserConfigExport} */
+export default async ({ host = process.env.TAURI_DEV_HOST }) => ({
+  clearScreen: false,
+  plugins: [(await import('@vitejs/plugin-react')).default()],
+  resolve: {
+    alias: { '@': (await import('path')).resolve(import.meta.dirname, 'src') },
+  },
+  server: {
+    strictPort: true,
+    host: host || false,
+    port: 1420,
+    hmr: !host
+      ? undefined
+      : {
+          host: host,
+          port: 1421,
+          protocol: 'ws',
+        },
+    watch: {
+      ignored: ['**/src-tauri/**'],
+    },
+  },
+})
